91ff0f60c03f1f5da1b025de10df791d2dd88d56
Thanakiat <87756732+Thanakiat@users.noreply.github.com>
2022-06-28 15:01:33 +0700

    Update Config.js

--- test/src/config/Config.js
+++ test/src/config/Config.js
@@ -1,4 +1,4 @@
 export const IP_BACKEND = "http://127.0.0.1:3001"
-export const EMAIL_SERVICE_ID = 'service_q7deyyn'
-export const EMAIL_TAMPLATE_ID = 'template_25qm7h3'
-export const EMAIL_PUBLIC_KEY = 'c7T_mr6usUbiH3jKh'
+export const EMAIL_SERVICE_ID = "EMAIL_SERVICE_ID"
+export const EMAIL_TAMPLATE_ID = "EMAIL_TAMPLATE_ID"
+export const EMAIL_PUBLIC_KEY = "EMAIL_PUBLIC"
